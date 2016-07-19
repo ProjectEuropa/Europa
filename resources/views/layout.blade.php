ebe85b5df80b63a6ccbd29d68f065d20b0507225
masatomiyamoto <masatomiyamoto@192.168.1.12>
2016-07-19 16:05:27 +0900

    composerを追加:Laravel HTML/Formタグを使用可能とする ソート処理を実装 削除ボタンクリック時の確認ダイアログを追加

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,8 @@
 
         <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/cerulean/bootstrap.min.css" rel="stylesheet" id="themesid">
         <link href="css/common.css" rel="stylesheet">
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <!--<style>
             html, body {
                 height: 100%;
@@ -80,7 +81,5 @@
                 <p>&copy; 2016 Team Project Europa <br>
             </footer>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     </body>
 </html>
