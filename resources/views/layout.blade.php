748c640d0cc94b37b5ba67d0c11e7f5a68ca1a01
masatomiyamoto <masatomiyamoto@192.168.1.12>
2016-07-24 21:13:49 +0900

    Sign inを消さずに表示。

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,9 +37,9 @@
 
                     <ul class="nav navbar-nav navbar-right">
                         @if (Auth::guest())
-                        @else
-                        {{-- 未ログイン時はSign in表示 認証機能未実装のため常に非表示 --}}
+                        {{-- 未ログイン時はSign in表示 --}}
                             <a class="navbar-brand" href="{!! url('/twitter') !!}">Sign in</a>
+                        @else
                         {{-- ログイン中はSign Out マイページ表示--}}
 	                    <li class="dropdown">
 	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
