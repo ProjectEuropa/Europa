bd579fe02c6c239e7b4937bde1a0e6834106affd
masatomiyamoto <masatomiyamoto@192.168.1.12>
2016-08-12 17:06:41 +0900

    EventBusinessをEventServiceに変更

--- app/BusinessService/EventService.php
+++ app/BusinessService/EventService.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\BusinessModel;
+namespace App\BusinessService;
 
 use Illuminate\Http\Request;
 use App\Event;
 
 /* 
- * EventNoticeビジネスクラス
+ * EventServieクラス
  * Eventモデルに関わるロジックを記述
  */
-class EventBusiness
+class EventService
 {
      /**
      * 
@@ -25,8 +25,13 @@ class EventBusiness
         $event->event_details = $request->input('eventDetails');
         $event->event_reference_url = $request->input('eventReferenceUrl');
         $event->event_type = $request->input('eventType');
-        $event->event_closing_day = $request->input('eventClosingDay');
-        $event->event_displaying_day = $request->input('eventDisplayingDay');
+        
+        // イベント締切、表示日時はyyyy/mm/dd + hh:mmで登録する
+        $eventClosingDay = $request->input('eventClosingDate').' '.$request->input('eventClosingTime');
+        $eventDisplayingDay = $request->input('eventDisplayingDate').' '.$request->input('eventDisplayingTime');
+        
+        $event->event_closing_day = $eventClosingDay;
+        $event->event_displaying_day = $eventDisplayingDay;
         
         if($event->save()){
             return true;
