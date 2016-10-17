80bdfc5ce7473681666f0ea7852715e54d5afa4b
masatomiyamoto <masatomiyamoto@192.168.1.12>
2016-10-17 12:13:21 +0900

    チームマッチ検索・まとめてDLリンクの表示をスライドダウン方式へ変更

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,67 +21,76 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="{!! url('/') !!}">Top/</a>
-                        <a class="navbar-brand" href="{!! url('/infomation') !!}">Infomation/</a>
-                        <a class="navbar-brand" href="{!! url('/search/team') !!}">Team data/</a>
-                        <a class="navbar-brand" href="{!! url('/search/match') !!}">Match Data/</a>
-                        @if (Auth::guest())
-                        {{-- 未ログイン時は簡易アップロード表示 --}}
-                            <a class="navbar-brand" href="{!! url('/simpleUpload') !!}">Simple Upload/</a>
-                        @else
-                        {{-- ログイン中は通常アップロード・イベント告知表示 --}}
+                        <ul class="nav navbar-nav navbar-left">
+                            <a class="navbar-brand" href="{!! url('/') !!}">Top/</a>
+                            <a class="navbar-brand" href="{!! url('/infomation') !!}">Infomation/</a>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    Search/
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="/search/team">Team data</a></li>
+                                    <li><a href="/search/match">Match Data</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle navbar-brand" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    Sum DL/
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="/sumdownload/team">Team DL</a></li>
+                                    <li><a href="/sumdownload/match">Match DL</a></li>
+                                </ul>
+                            </li>
+
+                            @if (Auth::guest())
+                            {{-- 未ログイン時は簡易アップロード表示 --}}
+                            <a class="navbar-brand" href="{!! url('/simpleupload') !!}">Simple Upload/</a>
+                            @else
+                            {{-- ログイン中は通常アップロード・イベント告知表示 --}}
                             <a class="navbar-brand" href="{!! url('/upload') !!}">Upload/</a>
-                            <a class="navbar-brand" href="{!! url('/eventNotice') !!}">Event Notice/</a>
-                        @endif
-                        <a class="navbar-brand" href="{!! url('/help') !!}">Help/</a>
-                        <a class="navbar-brand" href="{!! url('/links') !!}">Links</a>
+                            <a class="navbar-brand" href="{!! url('/eventnotice') !!}">Event Notice/</a>
+                            @endif
+                            <a class="navbar-brand" href="{!! url('/help') !!}">Help/</a>
+                            <a class="navbar-brand" href="{!! url('/links') !!}">Links</a>
+                        </ul>
                     </div>
 
                     <ul class="nav navbar-nav navbar-right">
                         @if (Auth::guest())
                         {{-- 未ログイン時はSign in表示 --}}
-                            <a class="navbar-brand" href="{!! url('/twitter') !!}">Sign in</a>
+                        <a class="navbar-brand" href="{!! url('/twitter') !!}">Sign in</a>
                         @else
                         {{-- ログイン中はSign Out マイページ表示--}}
-	                    <li class="dropdown">
-	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-	                            Sign in: {{ Auth::user()->name }} 
-	                            <img src="{{ Auth::user()->avatar }}" height="20" width="20"/>
-	                            <span class="caret"></span>
-	                        </a>
-	                        <ul class="dropdown-menu" role="menu">
-                                    <li><a href="/mypage">My Page</a></li>
-	                            <li><a href="/auth/logout">Sign out</a></li>
-	                        </ul>
-	                    </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Sign in: {{ Auth::user()->name }} 
+                                <img src="{{ Auth::user()->avatar }}" height="20" width="20"/>
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="/mypage">My Page</a></li>
+                                <li><a href="/auth/logout">Sign out</a></li>
+                            </ul>
+                        </li>
                         @endif
                     </ul>
-
-                    <!--<div id="navbar" class="navbar-collapse collapse">
-                      <form class="navbar-form navbar-right">
-                        <div class="form-group">
-                          <input type="text" placeholder="Email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                          <input type="password" placeholder="Password" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Sign in</button>
-                      </form>
-                    </div>--><!--/.navbar-collapse -->
                 </div>
             </nav>
         </header>
-        
+
 
         <div class="container main">
             {{-- 子のビューで指定される、contentセクションを読み込む --}}
             @yield('content')
+        </div>
 
+        <div class="container">
             <footer>
                 <p>&copy; 2016 Team Project Europa <br>
             </footer>
         </div>
-        
+
         <!-- 固有のJavaScript読み込み-->
         @yield('js')
     </body>
