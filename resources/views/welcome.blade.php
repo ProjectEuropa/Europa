07b43bf25c4100f5db0b6c0d115c34e66c8babdb
masatomiyamoto <masatomiyamoto@192.168.1.12>
2016-10-19 15:46:53 +0900

    タイトル文字列修正 Exa->EXA リンク先->各種リンク先

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
             <img src="image/Europa.jpg">
             <div class="carousel-caption">
                 <h1>Welcome To Europa</h1>
-                <p>Carnage Heart Exa 非公式アップローダーEuropaにようこそ！</p>
-                <p>Headerのリンク先からお進みください。</p>
+                <p>Carnage Heart EXA 非公式アップローダーEuropaにようこそ</p>
+                <p>Headerの各種リンク先からお進みください。</p>
                 <a href="https://twitter.com/share" class="twitter-share-button" data-size="large">Tweet</a>
                 <script>!function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location)?'http':'https'; if (!d.getElementById(id)){js = d.createElement(s); js.id = id; js.src = p + '://platform.twitter.com/widgets.js'; fjs.parentNode.insertBefore(js, fjs); }}(document, 'script', 'twitter-wjs');</script>
                 
