cbc33997f0d78c03c77f0aa82d15c32afb18a679
masatomiyamoto <masatomiyamoto@192.168.1.12>
2016-11-28 14:23:23 +0900

    削除失敗メッセージを追加

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -6,6 +6,7 @@ return [
     'run_mistake' => ':nameに誤りがあります。',
     'register_fail' => ':nameの登録に失敗しました。',
     'register_complete' => ':nameの登録が完了しました。',
+    'delete_fail' => ':nameの削除に失敗しました。',
     'delete_complete' => ':nameの削除が完了しました。',
     'upload_complete' => ':nameのアップロードが完了しました。',
     'update_complete' => ':nameの更新が完了しました。'
