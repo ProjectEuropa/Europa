32a32ca231cf5ad1cdf6194ab043c16671c9ff4a
Miyamoto Masato <Miyamoto Masato@M2>
2017-06-07 14:38:02 +0900

    Heroku用設定

--- app_boot.sh
+++ app_boot.sh
@@ -4,5 +4,4 @@
 /app/.heroku/php/bin/php /app/artisan clear-compiled
 /app/.heroku/php/bin/php /app/artisan optimize
 
-
-web: vendor/bin/heroku-php-nginx -i custom_php.ini -C nginx_app.conf public/
+vendor/bin/heroku-php-nginx -i custom_php.ini -C nginx_app.conf public/
