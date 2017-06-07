aa1b143f4b170895bcca5e0083564402348d7a8e
Miyamoto Masato <Miyamoto Masato@M2>
2017-06-07 11:55:13 +0900

    ffmpeg設定をHeroku用に変更

--- config/laravel-ffmpeg.php
+++ config/laravel-ffmpeg.php
@@ -3,10 +3,10 @@
 return [
     'default_disk' => 'local',
 
-    'ffmpeg.binaries' => '/usr/local/bin/ffmpeg',//'C:/ffmpeg/bin/ffmpeg.exe',
+    'ffmpeg.binaries' => '/app/vendor/bin/ffmpeg',//'C:/ffmpeg/bin/ffmpeg.exe',
     'ffmpeg.threads'  => 12,
 
-    'ffprobe.binaries' => '/usr/local/bin/ffprobe',//'C:/ffmpeg/bin/ffprobe.exe',//
+    'ffprobe.binaries' => '/app/vendor/bin/ffprobe',//'C:/ffmpeg/bin/ffprobe.exe',//
 
     'timeout' => 3600,
 ];
