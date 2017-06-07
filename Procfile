6619e9386d6dc035c29fea30c6b8791557e41fa3
Miyamoto Masato <Miyamoto Masato@M2>
2017-06-07 12:28:53 +0900

    heroku用のpublicディレクトリ設定に間違いがあったので修正

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: sh app_boot.sh
 worker: php artisan queue:listen
-web: vendor/bin/heroku-php-apache2 -i custom_php.ini .
--- app_boot.sh
+++ app_boot.sh
@@ -4,5 +4,9 @@
 /app/.heroku/php/bin/php /app/artisan clear-compiled
 /app/.heroku/php/bin/php /app/artisan optimize
 
+#custom_php_setting
+vendor/bin/heroku-php-apache2 -i custom_php.ini .
+
+
 # Boot up!
 vendor/bin/heroku-php-apache2 public/
