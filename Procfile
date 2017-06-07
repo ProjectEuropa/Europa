206aca9205aeb5d26768fc125aa0a803fa38a051
Miyamoto Masato <Miyamoto Masato@M2>
2017-06-07 12:34:09 +0900

    heroku用publicディレクトリ再修正

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
+web:vendor/bin/heroku-php-apache2 -i custom_php.ini .
 web: sh app_boot.sh
 worker: php artisan queue:listen
--- app_boot.sh
+++ app_boot.sh
@@ -4,9 +4,5 @@
 /app/.heroku/php/bin/php /app/artisan clear-compiled
 /app/.heroku/php/bin/php /app/artisan optimize
 
-#custom_php_setting
-vendor/bin/heroku-php-apache2 -i custom_php.ini .
-
-
 # Boot up!
 vendor/bin/heroku-php-apache2 public/
