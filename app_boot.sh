71e940c059003e44119224be4998954ddaa67927
Miyamoto Masato <Miyamoto Masato@M2>
2017-06-07 14:47:31 +0900

    Heroku用設定

--- app_boot.sh
+++ app_boot.sh
@@ -5,4 +5,5 @@
 /app/.heroku/php/bin/php /app/artisan optimize
 
 
-vendor/bin/heroku-php-apache2 -i .custom_php.ini vendor/bin/heroku-php-apache2 public/
+vendor/bin/heroku-php-apache2 -i .custom_php.ini 
+vendor/bin/heroku-php-apache2 public/
