ec504da36685c6a70b09e9c078294169d8e4de45
Miyamoto Masato <Miyamoto Masato@M2>
2017-06-07 14:12:16 +0900

    Heroku用設定

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
-web: vendor/bin/heroku-php-apache2 public/
-web: vendor/bin/heroku-php-apache2 -i .user.ini .
 web: sh app_boot.sh
 worker: php artisan queue:listen
--- app_boot.sh
+++ app_boot.sh
@@ -3,3 +3,7 @@
 # Artisan commands
 /app/.heroku/php/bin/php /app/artisan clear-compiled
 /app/.heroku/php/bin/php /app/artisan optimize
+
+
+vendor/bin/heroku-php-apache2 public/
+vendor/bin/heroku-php-apache2 -i .user.ini .
