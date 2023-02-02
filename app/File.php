b5ca3a9c0cdcf5a1bfcf5b90d6bca22da37e8645
Masato Miyamoto <m2delta29@gmail.com>
2023-02-02 17:08:50 +0900

    update: laravel 9

--- app/File.php
+++ app/File.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 class File extends Model
 {
     protected $table = "files";
+
+    public function createdAt(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => Carbon::parse($value)->timezone('Asia/Tokyo')->format('Y-m-d H:i')
+        );
+    }
 }
