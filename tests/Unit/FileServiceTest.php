5d743050598e507d10495955cc1ce28c9db86bc3
Masato Miyamoto <m2delta29@gmail.com>
2023-04-20 00:14:16 +0900

    add: test修正

--- tests/Unit/FileServiceTest.php
+++ tests/Unit/FileServiceTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class FileServiceTest extends TestCase
@@ -25,48 +26,59 @@ class FileServiceTest extends TestCase
 
     public function testRegisterFileData()
     {
-        // Set up a test user
         $user = User::factory()->create();
         Auth::login($user);
-
-        // Set up a test file
         Storage::fake('local');
-        $testFile = UploadedFile::fake()->create('testfile.CHE', 100);
-
-        // Set up a test request
-        $request = new \Illuminate\Http\Request();
-        $request->setMethod('POST');
-        $request->files->add(["teamFile" => $testFile]);
-
-        // Set up input data
-        $request->merge([
-            'teamOwnerName' => 'Test Owner',
-            'teamComment' => 'Test Comment',
-            'teamDeletePassWord' => 'Test Password',
-            'teamSearchTags' => 'tag1,tag2,tag3,tag4',
-        ]);
-
-        // Call the registerFileData method
-        $options = [
-            'isTeam' => true,
-            'isNormalUpdate' => true,
+        $testCases = [
+            ['isTeam' => true, 'isNormalUpdate' => true],
+            ['isTeam' => true, 'isNormalUpdate' => false],
+            ['isTeam' => false, 'isNormalUpdate' => true],
+            ['isTeam' => false, 'isNormalUpdate' => false],
         ];
-        $this->fileService->registerFileData($request, $options);
 
-        // Assert that the file was added to the database
-        $this->assertDatabaseHas('files', [
-            'file_name' => 'testfile.CHE',
-            'upload_owner_name' => 'Test Owner',
-            'file_comment' => 'Test Comment',
-            'delete_password' => 'Test Password',
-            'data_type' => '1',
-            'upload_user_id' => $user->id,
-            'search_tag1' => 'tag1',
-            'search_tag2' => 'tag2',
-            'search_tag3' => 'tag3',
-            'search_tag4' => 'tag4',
-        ]);
+        foreach ($testCases as $testCase) {
+            $options = [
+                'isTeam' => $testCase['isTeam'],
+                'isNormalUpdate' => $testCase['isNormalUpdate'],
+            ];
+            $request = new \Illuminate\Http\Request();
+            $request->setMethod('POST');
+            $dataType = $options['isTeam'] ? 'team' : 'match';
+            $dataTypeId = $dataType === 'team' ? '1' : '2';
+            $uploadUserId = $options['isNormalUpdate'] ? $user->id : 0;
+
+            $testFile = UploadedFile::fake()->create('testfile.CHE', 100);
+
+            $request->files->add(["{$dataType}File" => $testFile]);
+
+            $owner = Str::random(10);
+            $commnent = Str::random(10);
+            $password = Str::random(10);
+
+            $request->merge([
+                "{$dataType}OwnerName" => $owner,
+                "{$dataType}Comment" => $commnent,
+                "{$dataType}DeletePassWord" => $password,
+                "{$dataType}SearchTags" => 'tag1,tag2,tag3,tag4',
+            ]);
+
+            $this->fileService->registerFileData($request, $options);
+
+            $this->assertDatabaseHas('files', [
+                'file_name' => 'testfile.CHE',
+                'upload_owner_name' => $owner,
+                'file_comment' => $commnent,
+                'delete_password' => $password,
+                'data_type' => $dataTypeId,
+                'upload_user_id' => $uploadUserId,
+                'search_tag1' => 'tag1',
+                'search_tag2' => 'tag2',
+                'search_tag3' => 'tag3',
+                'search_tag4' => 'tag4',
+            ]);
+        }
     }
+
     public function it_stores_binary_data_correctly()
     {
         $filePath = storage_path("app/public/sample.CHE");
