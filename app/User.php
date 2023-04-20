fb08de294d2c5349d45d4b449b2bb78b7215d894
Masato Miyamoto <m2delta29@gmail.com>
2023-04-20 11:28:38 +0900

    fix

--- app/User.php
+++ app/User.php
@@ -6,7 +6,6 @@ use App\Notifications\CustomResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class User extends Authenticatable
 {
@@ -18,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'api_token'
+        'name', 'email', 'password', 'api_token',
     ];
 
     /**
