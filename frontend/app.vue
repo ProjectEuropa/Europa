e644dab3e67786a46a99935543b793c8562e48a9
Masato Miyamoto <m2delta29@gmail.com>
2023-12-06 14:26:37 +0900

    mod: node version

--- frontend/app.vue
+++ frontend/app.vue
@@ -25,9 +25,7 @@ const items = [
 <template>
   <v-card>
     <v-layout>
-      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
-
-      <v-app-bar color="primary" prominent>
+      <v-app-bar app clipped-left color="blue darken-3 white--text" class="main__blue">
         <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
         <v-toolbar-title>My files</v-toolbar-title>
@@ -45,12 +43,27 @@ const items = [
         <v-list :items="items"></v-list>
       </v-navigation-drawer>
 
-      <v-main style="height: 500px;">
+      <v-main style="">
         <v-card-text>
           The navigation drawer will appear from the bottom on smaller size screens.
         </v-card-text>
       </v-main>
+
+      <v-footer app clipped-center color="blue darken-3 white--text" class="main__blue">
+        <span>&copy; Team Project Europa 2016-{{new Date().getFullYear()}}</span>
+      </v-footer>
     </v-layout>
   </v-card>
 </template>
+
+<style>
+.main__blue {
+  background-color: #1565c0!important;
+  border-color: #1565c0!important;
+  border-top-color: rgb(21, 101, 192) !important;
+  border-right-color: rgb(21, 101, 192) !important;
+  border-bottom-color: rgb(21, 101, 192) !important;
+  border-left-color: rgb(21, 101, 192) !important;
+}
+</style>
 
